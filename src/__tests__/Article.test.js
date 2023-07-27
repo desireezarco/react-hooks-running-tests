ff05906f0000249eb432798fcaef248a156bd349
Desiree Zarco <desireeannzarco7@gmail.com>
2023-07-27 14:42:16 -0700

    passed tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -9,7 +9,7 @@ test("displays the text 'please pass this test'", () => {
 
   const element = screen.queryByText("please pass this test");
 
-  // screen.debug(element);
+  screen.debug(element);
 
   expect(element).toBeInTheDocument();
 });
